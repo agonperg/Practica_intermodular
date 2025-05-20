6a8b76d2c62f040cbc9b157e93c6c61bc37f71df
pnh0002 <pnh0002@alu.medac.es>
2025-05-20 10:21:33 +0200

    Avence en la creacion de base de datos

---
+++ Programación/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Cliente.java
@@ -0,0 +1,10 @@
+
+
+package com.mycompany.proyecto_intermodular;
+
+
+public class Cliente {
+
+    
+    
+}
--- Programación/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Proyecto_Intermodular.java
+++ Programación/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Proyecto_Intermodular.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.proyecto_intermodular;
 
+import java.util.Scanner;
+
 /**
  *
  * @author PC220
@@ -11,6 +13,21 @@ package com.mycompany.proyecto_intermodular;
 public class Proyecto_Intermodular {
 
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in); 
+        String opcion = ""; 
+        
+        do{
+            System.out.println("[1] Ver los datos de los clientes. ");
+            System.out.println("[2] Seleccione una de las listas. ");
+            System.out.println("[3] Ver las entidades secundarias. ");
+            System.out.println("[0] Salir. ");
+            opcion = sc.nextLine(); 
+            
+            
+            
+            
+        }while(!(opcion.equals("0"))); 
         
         
         
