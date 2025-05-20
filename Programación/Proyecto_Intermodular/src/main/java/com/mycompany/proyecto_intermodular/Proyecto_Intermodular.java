19276eb9520e915e51044ebd93ddc3579268b56e
pnh0002 <pnh0002@alu.medac.es>
2025-05-20 09:42:27 +0200

    Creación de programación

---
+++ Programación/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/Proyecto_Intermodular.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_intermodular;
+
+/**
+ *
+ * @author PC220
+ */
+public class Proyecto_Intermodular {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
---
+++ Programación/Proyecto_Intermodular/src/main/java/com/mycompany/proyecto_intermodular/conexion/Conexion.java
@@ -0,0 +1,8 @@
+
+
+package com.mycompany.proyecto_intermodular.conexion;
+
+
+public class Conexion {
+
+}
